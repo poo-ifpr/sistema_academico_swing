9f3a709e6e598dfd60a2c00cfd621d090c441cbc
Everaldo Gomes <everaldo.gomes@gmail.com>
2016-11-20 23:28:22 -0200

    Renomeado pacote para ui

--- src/ui/SistemaAcademicoUI.java
+++ src/ui/SistemaAcademicoUI.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package sistemaacademicoui;
+package ui;
 
 import facade.FacadeCurso;
 import java.awt.Point;
